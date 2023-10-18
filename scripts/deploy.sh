38cb264b8ef0f1f72caede07a8c97ef311a3b9df
Ubugeeei <ubuge1122@gmail.com>
2023-10-18 15:17:46 +0900

    other: #7 🔹 deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+pnpm publish packages/tecack --access=public
+
+pnpm publish packages/frontend --access=public
+
+pnpm publish packages/backend --access=public
+
+pnpm publish packages/dataset --access=public
+
+pnpm publish packages/shared --access=public
+
+pnpm publish packages/tools --access=public
