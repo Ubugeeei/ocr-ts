0b66e306749e548a0c11cae298edf99e9e8c9530
Ubugeeei <ubuge1122@gmail.com>
2023-11-28 17:45:28 +0900

    feat: 💪 (type) TecackStrokes

--- packages/shared/src/stroke.ts
+++ packages/shared/src/stroke.ts
@@ -1,9 +1,18 @@
-export type TecackDataset = [TargetChar, TargetCharStrokeLength, Array<TecackStroke>];
+/**
+ * The TecackDataset.
+ *
+ * @example ["字", 6, [..]]
+ */
+export type TecackDataset = [TargetChar, TargetCharStrokeLength, TecackStrokes];
 
 export type TargetChar = string;
 
 export type TargetCharStrokeLength = number;
 
+/** Stroke information for any single character. */
+export type TecackStrokes = Array<TecackStroke>;
+
+/** Information on a single stroke of any single character. */
 export type TecackStroke = Array<Position>;
 
 export type Position = [X, Y];
